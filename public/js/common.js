7bd2f62c9ff23b8bcd2db4489af71e813bc47e51
smohite03 <smohite@qodeleaf.com>
2023-03-09 19:21:25 +0530

    Frontend Pages Completed - Cart - Index - Category Customer- - Profile - Order Seller - Dashboard - Orders

--- public/js/common.js
+++ public/js/common.js
@@ -57,17 +57,45 @@ function login()
         data: user,
         dataType: "json",
         success: function(resultData){
-            alert("Successfully logged in");
             sessionStorage.setItem("UserID", resultData.session.id);
             sessionStorage.setItem("email", resultData.session.email);
             sessionStorage.setItem("role", resultData.session.role);
             sessionStorage.setItem("Token", resultData.token);
-        }
+            if(resultData.session.role == "Customer"){
+                window.location.href = "/crm/customer/dashboard";
+            }else if(resultData.session.role == "Seller"){
+                window.location.href = "/crm/seller/dashboard";
+            }
+        }, error: function(XMLHttpRequest, textStatus, errorThrown) {
+                alert("Please check your email and password try again");
+         }
     });
 }
-function genrateReport()
+function getCustomerDashboarddata()
 {
-   
+    var userid = sessionStorage.getItem("UserID");
+    var token = sessionStorage.getItem("Token");
+    if(userid == null || token == null){
+        alert("Unauthorized User Please Login");
+        window.location.href = "/crm";
+    }
+}
+function getsellerDashboarddata()
+{
+    var userid = sessionStorage.getItem("UserID");
+    var token = sessionStorage.getItem("Token");
+    if(userid == null || token == null){
+        alert("Unauthorized User Please Login");
+        window.location.href = "/crm";
+    }
+}
+function setSession(){
+    var userid = sessionStorage.getItem("UserID");
+    var token = sessionStorage.getItem("Token");
+    if(userid == null || token == null){
+        alert("Unauthorized User Please Login");
+        window.location.href = "/crm";
+    }
 }
 function submitCustomerprofile()
 {
@@ -180,7 +208,12 @@ function getCustomerProdiledata(){
             $('#city').val(resultData.city);
             $('#state').val(resultData.state);
             $('#address').val(resultData.area);
-        }
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            if(errorThrown == "Forbidden"){
+                window.location.href = "/crm";
+            }
+         }
     });
 }
 
@@ -201,7 +234,12 @@ function getSellerProfiledata(){
             $('#state').val(resultData.state);
             $('#address').val(resultData.shopArea);
             $('#gst').val(resultData.gstNum);
-        }
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            if(errorThrown == "Forbidden"){
+                window.location.href = "/crm";
+            }
+         }
     });
 }
 
@@ -223,6 +261,31 @@ function getallproduct()
         }
     });
 }
+
+function getallproductbyCategory()
+{
+    const urlParams = new URLSearchParams(location.search);
+    let category = 0;
+    for (const [key, value] of urlParams) {
+        category = value;
+    }
+    $('#category').html(category);
+    var productArray = [];
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:3000/product/category?category="+category+"",
+        dataType: "json",
+        success: function(resultData){
+            Object.keys(resultData).forEach(key => {
+                console.log(resultData[key]);
+                product = '<div class="col-lg-3 col-md-4 col-sm-6 pb-1"><div class="card product-item border-0 mb-4"><div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0"><img class="img-fluid w-100" src="'+resultData[key].imgpath+'" alt=""></div><div class="card-body border-left border-right text-center p-0 pt-4 pb-3"><h6 class="text-truncate mb-3">'+resultData[key].productName+'</h6><div class="d-flex justify-content-center"><h6>Rs '+resultData[key].productRate+' </h6></div></div><a style="text-align: center" href="http://localhost:8000/detail?id='+resultData[key].id+'" class="card-footer border">View Product Details</a></div></div>';
+                productArray = productArray + product;
+            });
+            $('#products').html(productArray);
+        }
+    });
+}
+
 function showproductdetail(){
     const urlParams = new URLSearchParams(location.search);
     let id = 0;
@@ -234,11 +297,12 @@ function showproductdetail(){
         url: "http://localhost:3000/product/item/?id="+id+"",
         dataType: "json",
         success: function(resultData){
-            product = '<div class="col-lg-5 pb-5"><div id="product-carousel" class="carousel slide" data-ride="carousel"><div class="carousel-inner border"><div class="carousel-item active"><img class="w-100 h-100" src="'+resultData[0].imgpath+'" alt="Image"></div></div></div></div><div class="col-lg-7 pb-5"><h3 class="font-weight-semi-bold">'+resultData[0].productName+'</h3><h3 class="font-weight-semi-bold mb-4">Rs '+resultData[0].productRate+'</h3><p class="mb-4">'+resultData[0].productDiscription+'</p></p><div class="d-flex align-items-center mb-4 pt-2"><div class="input-group quantity mr-3" style="width: 130px;"><div class="input-group-btn"><input type="text" class="form-control bg-secondary text-center" value="1"></div><button class="btn btn-primary px-3"><i class="fa fa-shopping-cart mr-1"></i> Add To Cart</button></div></div>';
+            product = '<div class="col-lg-5 pb-5"><div id="product-carousel" class="carousel slide" data-ride="carousel"><div class="carousel-inner border"><div class="carousel-item active"><img class="w-100 h-100" src="'+resultData[0].imgpath+'" alt="Image"></div></div></div></div><div class="col-lg-7 pb-5"><h3 class="font-weight-semi-bold">'+resultData[0].productName+'<input value="'+resultData[0].productName+'" id="productid" hidden></h3><h3 class="font-weight-semi-bold mb-4">Rs '+resultData[0].productRate+'</h3><p class="mb-4">'+resultData[0].productDiscription+'</p></p><div class="d-flex align-items-center mb-4 pt-2"><div class="input-group quantity mr-3" style="width: 130px;"><div class="input-group-btn"><input type="text" class="form-control bg-secondary text-center" id="quantity" value="1"></div><button class="btn btn-primary px-3" onclick="addtoCart('+resultData[0].id+')"><i class="fa fa-shopping-cart mr-1"></i> Add To Cart</button></div></div><input id="productrate" value = "'+resultData[0].productRate+'" hidden/> ';
             $('#products').html(product);
         }
     });
 }
+
 function showAllproduct()
 {
     var productArray = [];
@@ -253,9 +317,15 @@ function showAllproduct()
                 productArray = productArray + product;
             });
             $('#productSeller').html(productArray);
-        }
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            if(errorThrown == "Forbidden"){
+                window.location.href = "/crm";
+            }
+         }
     });
 }
+
 function deleteProduct(ProductId){
     $.ajax({
         type: "DELETE",
@@ -267,15 +337,21 @@ function deleteProduct(ProductId){
         success: function(resultData){
             alert(resultData);
             window.location.href = "/crm/seller/products";
-        }
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            if(errorThrown == "Forbidden"){
+                window.location.href = "/crm";
+            }
+         }
     });
 }
+
 function getOrderbySeller()
 {
-    var orderArray = [];
-    var customername;
+    let customername;
     var customerphone;
     var customeraddress;
+    var orderArray = [];
     $.ajax({
         type: "GET",
         url: "http://localhost:3000/order/seller?sellerId="+sessionStorage.getItem("UserID")+"",
@@ -285,7 +361,6 @@ function getOrderbySeller()
         },
         success: function(resultData){
             Object.keys(resultData).forEach(key => {
-                console.log(resultData);
                 $.ajax({
                     type: "GET",
                     url: "http://localhost:3000/customer/profile?customerId="+resultData[key].custId+"",
@@ -297,14 +372,19 @@ function getOrderbySeller()
                         customername = resultCustData.fullName;
                         customerphone = resultCustData.phoneNumber;
                         customeraddress = resultCustData.area +' '+ resultCustData.city;
-                        alert(customername);
+                        order = '<tr><td><button onclick="showProductsofOrder('+resultData[key].id+');" data-toggle="modal" data-target="#productModal" class="btn btn-sm btn-info">'+resultData[key].id+'</button></td><td>'+customername+'</td><td>'+resultData[key].createdAt+'</td><td>'+customeraddress+'</td><td>'+customerphone+'</td><td><button class="btn btn-sm btn-warning">'+resultData[key].orderStatus+'</button></td><td><button class="btn btn-dark btn-sm" onclick="deliveredOrder('+resultData[key].id+')">Delivered</button><button class="btn btn-sm btn-danger" onclick="cancelOrder('+resultData[key].id+')">Cancel</button></td></tr>';
+                        orderArray = orderArray + order;
+                        $('#orderSeller').html(orderArray);
                     }
                 });
-                order = '<tr><td><button class="btn btn-sm btn-info">'+resultData[key].id+'</button></td><td>'+customername+'</td><td>'+resultData[key].createdAt+'</td><td>'+customeraddress+'</td><td><button class="btn btn-sm btn-warning">'+resultData[key].orderStatus+'</button></td><td><button class="btn btn-dark btn-sm" onclick="deliveredOrder('+resultData[key].id+')">Delivered</button><button class="btn btn-sm btn-danger" onclick="cancelOrder('+resultData[key].id+')">Cancel</button></td></tr>';
-                orderArray = orderArray + order;
             });
-            $('#orderSeller').html(orderArray);
-        }
+
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            if(errorThrown == "Forbidden"){
+                window.location.href = "/crm";
+            }
+         }
     });
 }
 
@@ -324,13 +404,19 @@ function getOrderbyCustomer()
                 var date = new Date(resultData[key].createdAt);
                 date.setDate(date.getDate() + 7);
                 var product = String(resultData[key].productIds);
-                order = '<tr><td><button type="button" onclick="showProductsofOrder('+resultData[key].id+');" data-toggle="modal" data-target="#productModal"  class="btn btn-sm btn-info">'+resultData[key].id+'</button></td><td>'+resultData[key].OrderDesciption+'</td><td>'+resultData[key].createdAt+'</td><td>'+date+'</td><td><button class="btn btn-sm btn-warning">'+resultData[key].orderStatus+'</button></td><td><button class="btn btn-sm btn-danger" onclick="cancelOrder('+resultData[key].id+')">Cancel</button></td></tr>';
+                order = '<tr><td><button type="button" onclick="showProductsofOrder('+resultData[key].id+');" data-toggle="modal" data-target="#productModal"  class="btn btn-sm btn-info">'+resultData[key].id+'</button></td><td>'+resultData[key].OrderDesciption+'</td><td>'+resultData[key].createdAt+'</td><td>'+date+'</td><td><button class="btn btn-sm btn-warning">'+resultData[key].orderStatus+'</button></td></tr>';
                 orderArray = orderArray + order;
             });
             $('#orderCustomer').html(orderArray);
-        }
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            if(errorThrown == "Forbidden"){
+                window.location.href = "/crm";
+            }
+         }
     });
 }
+
 function showProductsofOrder(orderId)
 {
     var productArray = [];
@@ -355,6 +441,128 @@ function showProductsofOrder(orderId)
                     }
                 });
             }
-        }
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            if(errorThrown == "Forbidden"){
+                window.location.href = "/crm";
+            }
+         }
+    });
+}
+
+function addtoCart(ProductID)
+{
+    var userid = sessionStorage.getItem("UserID");
+    if(userid == null){
+        alert("Please Login Before To Add Product");
+        window.location.href = "/crm";
+    }
+    const quanitity = $('#quantity').val();
+    const productname = $('#productid').val();
+    const productrate = $('#productrate').val();
+    const cart = {
+        "custId": userid,
+        "productId": ProductID,
+        "productName": productname,
+        "productRate": productrate,
+        "productQuantity": quanitity
+      }
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:3000/cart",
+        headers: {
+            authorization: 'Bearer ' + sessionStorage.getItem("Token"),
+        },
+        data: cart,
+        dataType: "text",
+        success: function(resultData){
+            alert("Product Successfully Added TO Cart");
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            alert("Please Login Before Adding Product TO Cart");
+            if(errorThrown == "Forbidden"){
+                window.location.href = "/crm";
+            }
+         }
+    });
+}
+
+function viewCustomerCart() {
+    var productArr = [];
+    var total = 0;
+    var userid = sessionStorage.getItem("UserID");
+    if(userid == null){
+        alert("Please Login");
+        window.location.href = "/crm";
+    }
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:3000/cart/?custId="+sessionStorage.getItem("UserID")+"",
+        headers: {
+            authorization: 'Bearer ' + sessionStorage.getItem("Token"),
+        },
+        dataType: "json",
+        success: function(resultData){
+            Object.keys(resultData).forEach(key => {
+                total = total + resultData[key].productRate * resultData[key].productQuantity;
+                order = '<tr><td class="align-middle">'+resultData[key].productName+'</td><td class="align-middle">Rs '+resultData[key].productRate+'</td><td class="align-middle">'+resultData[key].productQuantity+'</td><td class="align-middle">Rs '+resultData[key].productRate * resultData[key].productQuantity+'</td><td class="align-middle"><button class="btn btn-sm btn-primary" onclick="removeItemfromCart()><i class="fa fa-times"></i>Delete</button></td></tr>';
+                productArr = productArr + order;
+                $('#total').html(total + ' Rs');
+                $('#cartvalues').html(productArr);
+            });
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            if(errorThrown == "Forbidden"){
+                alert("Unauthorize User Please Login");
+                window.location.href = "/crm";
+            }
+         }
+    });
+}
+
+function clearPastUserDetails()
+{
+    sessionStorage.clear();
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:3000/user/logout",
+        dataType: "json",
+        success: function(resultData){
+            console.log("Logout Sccessfully");
+        },
+    });
+}
+
+function checkoutOrder()
+{
+    var productArr = [];
+    var total = 0;
+    var userid = sessionStorage.getItem("UserID");
+    if(userid == null){
+        alert("Please Login");
+        window.location.href = "/crm";
+    }
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:3000/cart/?custId="+sessionStorage.getItem("UserID")+"",
+        headers: {
+            authorization: 'Bearer ' + sessionStorage.getItem("Token"),
+        },
+        dataType: "json",
+        success: function(resultData){
+            Object.keys(resultData).forEach(key => {
+                total = total + resultData[key].productRate * resultData[key].productQuantity;
+                order = '<div class="d-flex justify-content-between"><p>'+resultData[key].productName+' '+resultData[key].productQuantity+'</p><p>Rs '+resultData[key].productRate+'</p></div>'
+                productArr = productArr + order;
+                $('#total').html(total + ' Rs');
+                $('#cartvalues').html(productArr);
+            });
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            if(errorThrown == "Forbidden"){
+                alert("Unauthorize User Please Login");
+                window.location.href = "/crm";
+            }
+         }
     });
 }
